b05b114bd0af6dcd9056188a4dcac1b7d8c29d0b
ArianaVillegas <ariana.villegas@utec.edu.pe>
2021-04-28 23:32:00 -0500

    make it better

--- elemental/settings.py
+++ elemental/settings.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ${{ secrets.DJANGO_SECRET_KEY }}
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='Coloca la llave default que proporciona Django')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
