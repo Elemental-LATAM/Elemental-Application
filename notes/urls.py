374cc001ffa230d02b2e5d9b07ac4e0b56010c76
ArianaVillegas <ariana.villegas@utec.edu.pe>
2021-05-05 15:05:52 -0500

    Update notes urls

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from notes.views import PetitionListView
+from notes.views import *
 
 app_name = 'notes'
 urlpatterns = [
-    path('peticiones/', PetitionListView.as_view(), name='peticiones'),
+    path("peticiones/", PetitionListView.as_view(), name="peticiones"),
+    path("authorize-petition/<slug>/", authorize_petition, name='authorize-petition'),
+    path("deny-petition/<slug>/", deny_petition, name='deny-petition'),
+    path("cancel-petition/<slug>/", cancel_petition, name='cancel-petition'),
+    path("delete-notification/<slug>/", delete_notification, name="delete-notification"),
+    path("miembro/<slug>/send-invitation/", send_invitation_project, name='send-invitation-project'),
 ]
